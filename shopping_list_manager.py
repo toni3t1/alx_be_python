168013e218cac46a20dd01df73dfb8104a701e2d
gerald tony <tonygerald98@gmail.com>
2024-07-12 23:06:14 +0300

    Shopping list

---
+++ shopping_list_manager.py
@@ -0,0 +1,49 @@
+def display_menu():
+    print("\nShopping List Manager")
+    print("1. Add item")
+    print("2. Remove item")
+    print("3. View list")
+    print("4. Exit")
+
+def add_item(shopping_list):
+    item = input("Enter the item to add: ")
+    shopping_list.append(item)
+    print(f"'{item}' has been added to the list.")
+
+def remove_item(shopping_list):
+    item = input("Enter the item to remove: ")
+    if item in shopping_list:
+        shopping_list.remove(item)
+        print(f"'{item}' has been removed from the list.")
+    else:
+        print(f"'{item}' not found in the list.")
+
+def view_list(shopping_list):
+    if shopping_list:
+        print("\nCurrent Shopping List:")
+        for item in shopping_list:
+            print(f"- {item}")
+    else:
+        print("\nThe shopping list is empty.")
+
+def main():
+    shopping_list = []
+    
+    while True:
+        display_menu()
+        choice = input("Choose an option: ")
+        
+        if choice == '1':
+            add_item(shopping_list)
+        elif choice == '2':
+            remove_item(shopping_list)
+        elif choice == '3':
+            view_list(shopping_list)
+        elif choice == '4':
+            print("Exiting the Shopping List Manager. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+if __name__ == "__main__":
+    main()
